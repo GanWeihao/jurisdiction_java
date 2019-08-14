8c56aa36bea407d39178fa9747bcbe72e5f6f396
GanWeihao <18290605616@163.com>
2019-08-14 11:24:43 +0800

    整合spring+mybatis

--- src/main/java/org/item/jurisdiction/service/Impl/UserRoleServiceImpl.java
+++ src/main/java/org/item/jurisdiction/service/Impl/UserRoleServiceImpl.java
@@ -1,11 +1,9 @@
 package org.item.jurisdiction.service.Impl;
 
 import org.item.jurisdiction.mapper.UserRoleMapper;
-import org.item.jurisdiction.model.User;
 import org.item.jurisdiction.service.UserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 import java.util.List;
